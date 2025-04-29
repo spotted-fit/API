5827ddf1ca6279fd74c35d34cdc53f06afa2814b
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2025-04-29 12:04:38 +0200

    CORS support

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 import db.DatabaseFactory
 import io.ktor.http.*
-import io.ktor.serialization.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -10,6 +9,7 @@ import io.ktor.server.routing.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.server.plugins.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import models.ErrorResponse
@@ -55,6 +55,19 @@ fun main() {
             exception<Throwable> { call, cause ->
                 call.respond(ErrorResponse(message = "Internal server error. " + cause.message))
             }
+        }
+        install(CORS) {
+            allowMethod(HttpMethod.Options)
+            allowMethod(HttpMethod.Get)
+            allowMethod(HttpMethod.Post)
+            allowMethod(HttpMethod.Put)
+            allowMethod(HttpMethod.Delete)
+            allowMethod(HttpMethod.Patch)
+            allowHeader(HttpHeaders.Authorization)
+            allowHeader(HttpHeaders.ContentType)
+            allowHeader(HttpHeaders.AccessControlAllowOrigin)
+            allowCredentials = true
+            anyHost() // Allow requests from any host
         }
         routing {
             authRoutes()
