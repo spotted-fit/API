c7b0ecf8584779836fc9b30b678507b2964ac0bd
Georgii Gromov <43499418+giothun@users.noreply.github.com>
2025-05-05 06:25:06 +0200

    Revert "added host allow"
    This reverts commit 73b8d0b9b2a95633e23b20e13ebdf4b4ba0a7693.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -64,10 +64,6 @@ fun main() {
             }
         }
         install(CORS) {
-            allowHost("app.spotted.fit", schemes = listOf("https"))
-
-            allowCredentials = true
-
             allowMethod(HttpMethod.Options)
             allowMethod(HttpMethod.Get)
             allowMethod(HttpMethod.Post)
@@ -78,6 +74,9 @@ fun main() {
             allowHeader(HttpHeaders.ContentType)
             allowHeader(HttpHeaders.AccessControlAllowOrigin)
             allowHeader(HttpHeaders.Cookie)
+            exposeHeader(HttpHeaders.SetCookie)
+            allowCredentials = true
+            anyHost() // Allow requests from any host - consider restricting to specific origins for better security
         }
         routing {
             authRoutes()
