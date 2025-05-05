2f5dc1a1a9ed055c7b51571a34432b347403a9b9
Georgii Gromov <43499418+giothun@users.noreply.github.com>
2025-05-05 06:25:33 +0200

    removed redundant header and revert commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -74,7 +74,6 @@ fun main() {
             allowHeader(HttpHeaders.ContentType)
             allowHeader(HttpHeaders.AccessControlAllowOrigin)
             allowHeader(HttpHeaders.Cookie)
-            exposeHeader(HttpHeaders.SetCookie)
             allowCredentials = true
             anyHost() // Allow requests from any host - consider restricting to specific origins for better security
         }
